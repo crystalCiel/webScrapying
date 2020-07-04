859519efacc778d3628fc7c4085277909903bd7f
RichyYang <w4567892015@gmail.com>
2020-07-04 21:24:56 +0800

    fix cli function & add git ignore file

--- implementCLI/index.js
+++ implementCLI/index.js
@@ -5,51 +5,23 @@ let rl = readline.createInterface({
     input: process.stdin,
     output: process.stdout,
 });
-let topic = ['請輸入要搜尋的內容: ', '請輸入要搜尋開始日期(格式 yyyy/mm/dd ): ', '請輸入要搜尋結束日期(格式 yyyy/mm/dd ): '];
-let recodeSerchData = [];
 
-function question (topic, recodeArr ) {
-    return new Promise ((reslove, reject) => {
-        rl.question(topic, (answer) => {
-            recodeSerchData.push(answer);
-            reslove();
-        });
-    })
+async function questionSync(str) {
+    return new Promise((resolve)=>{
+        rl.question(str, resolve);
+    });
 }
 
-// 下面兩種匿名函數擇一使用 (相同作用不同寫法)
-// 方法一 : 波動拳法(??
-// (() => {
-//     rl.question(topic[0], (answer) => {
-//         recodeSerchData.push(answer);
-    
-//         rl.question(topic[1], (answer) => {
-//             recodeSerchData.push(answer);
-    
-//             rl.question(topic[2], (answer) => {
-//                 recodeSerchData.push(answer);
+async function questionArray (topic, recodeArr = []) {
+    if(topic.length === 0) return recodeArr;
+    recodeArr.push(await questionSync(topic.shift()));
+    return await questionArray(topic, recodeArr);
+}
 
-//                 console.log('資料正在搜尋中，請稍後...');
-//                 console.log('');
-
-//                 webCrawler.searchPlurk(recodeSerchData[0], recodeSerchData[1], recodeSerchData[2]);
-                
-//                 rl.close();
-//             });
-//         });
-//     });
-// }) ();
-
-// 方法二
 (async () => {
-    await question(topic[0]);
-    await question(topic[1]);
-    await question(topic[2]);
-
+    let topic = ['請輸入要搜尋的內容: ', '請輸入要搜尋開始日期(格式 yyyy/mm/dd ): ', '請輸入要搜尋結束日期(格式 yyyy/mm/dd ): '];
+    const recodeSearchData = await questionArray(topic);
     console.log('資料正在搜尋中，請稍後...');
-    console.log('');
-
-    await webCrawler.searchPlurk(recodeSerchData[0], recodeSerchData[1], recodeSerchData[2]);
-
+    await webCrawler.searchPlurk(...recodeSearchData);
     rl.close();
 }) ();
--- implementCLI/webScrapying.js
+++ implementCLI/webScrapying.js
@@ -1,40 +1,31 @@
 const axios = require('axios');
 const cheerio = require('cheerio');
 
+function _plurksFormat(result) {
+    const output = result.data.plurks.map((item) => {
+        const $ = cheerio.load(item.content);
+        return {
+            content: $.text(),
+            url: `https://www.plurk.com/p/${parseInt(item.plurk_id).toString(36)}`
+        };
+    });
+    console.log(output);
+}
+
 function searchPlurk (query, start_date, end_date) {
-
-    let serchData = {
-        query: query,
-        start_date: start_date,
-        end_date: end_date,
-    }
-
-    if (!serchData.query) {
+    const searchData = { query, start_date, end_date };
+    if (!searchData.query) {
         console.log('搜尋條件不正確.');
         return ;
-    } 
-    
+    }
     // 以 axios 方式來搜尋噗浪上的資料
     axios({
         method: 'post',
         url: 'https://www.plurk.com/Search/search2',
-        data: encodeURI(`query=${serchData.query}&start_date=${serchData.start_date}&end_date=${serchData.end_date}`),
+        data: encodeURI(`query=${searchData.query}&start_date=${searchData.start_date}&end_date=${searchData.end_date}`),
+    }).then(_plurksFormat).catch((err) => {
+        console.error('search error:', err);
     })
-        .then((result) => {
-            let target = result.data.plurks;
-            let output = target.map((item) => {
-                let $ = cheerio.load(item.content);
-                return {content: $.text(), url: plurkId(item.plurk_id)};
-            });
-            function plurkId (target) {
-                return `https://www.plurk.com/p/${parseInt(target).toString(36)}`;
-            }
-    
-            console.log(output);
-        })
-        .catch((err) => {
-            console.error('serch error:', err);
-        })
 }
 
 module.exports = {
